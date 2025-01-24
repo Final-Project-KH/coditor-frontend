243071a670d188b654930afc0de5a6790808df8a
Tea-Wha <htw7880@gmail.com>
2025-01-24 09:25:15 +0900

    autologin

--- src/react/pages/main/main.js
+++ src/react/pages/main/main.js
@@ -1,11 +1,21 @@
-import { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import Carousel from "./Carousel";
-import {
-  Wrap,
-} from "../../styles/main/Main";
+import {Wrap} from "../../styles/main/Main";
 import ScrollToTopButton from "../../styles/ScrollToTopButton";
+import {setAutoLogin} from "../../../redux/slice/loginSlice";
+import {useSelector, useDispatch} from "react-redux";
+import {setLoginData, setError} from "../../../redux/slice/authSlice";
+import Common from "../../../util/Common";
 
 const Main = () => {
+  const isautologin = useSelector((state) => state.login.isautologin);
+
+  useEffect(() => {
+    if (isautologin) {
+    } else {
+    }
+  }, []);
+
   return (
     <Wrap>
       <Carousel />
