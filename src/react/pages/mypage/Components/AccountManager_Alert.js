35d29d0cec37bd1db1f04a6e383a6a5b79ca8b12
thx-to <tlsekghk@gmail.com>
2025-02-06 20:22:11 +0900

    알림설정부분 로컬스토리지 저장 (임시)

--- src/react/pages/mypage/Components/AccountManager_Alert.js
+++ src/react/pages/mypage/Components/AccountManager_Alert.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   CenterContainerEach,
   CenterContainerTitle,
@@ -11,6 +12,42 @@ import {
 import AlertSwtich from "./AlertSwitch";
 
 const AccountManager_Alert = () => {
+  // alert상태 localstorage에 저장
+  const getStoredState = (key, defaultValue) => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue !== null ? JSON.parse(storedValue) : defaultValue;
+  };
+  const [commentAlert, setCommentAlert] = useState(() =>
+    getStoredState("commentAlert", false)
+  );
+  const [marketingAlert, setMarketingAlert] = useState(() =>
+    getStoredState("marketingAlert", false)
+  );
+  useEffect(() => {
+    localStorage.setItem("commentAlert", JSON.stringify(commentAlert));
+  }, [commentAlert]);
+  useEffect(() => {
+    localStorage.setItem("marketingAlert", JSON.stringify(marketingAlert));
+  }, [marketingAlert]);
+
+  const handleCommentAlert = (event) => {
+    setCommentAlert(event.target.checked); // ✅ event에서 checked 값 가져오기
+    alert(
+      `댓글 알림이 ${
+        event.target.checked ? "설정되었습니다" : "해제되었습니다"
+      }.`
+    );
+  };
+
+  const handleMarketingAlert = (event) => {
+    setMarketingAlert(event.target.checked);
+    alert(
+      `마케팅 알림이 ${
+        event.target.checked ? "설정되었습니다" : "해제되었습니다"
+      }.`
+    );
+  };
+
   return (
     <>
       <CenterContainerEach>
@@ -22,7 +59,10 @@ const AccountManager_Alert = () => {
               <AlertText>
                 내 질문에 댓글이 등록되면 알림을 받겠습니다.
               </AlertText>
-              <AlertSwtich />
+              <AlertSwtich
+                onChange={handleCommentAlert}
+                checked={commentAlert}
+              />
             </CenterAlertContentsInner>
           </CenterAlertContentsEach1>
           <CenterAlertContentsEach2>
@@ -32,7 +72,10 @@ const AccountManager_Alert = () => {
                 강의 오픈, 신규 테스트 등록, 각종 이벤트, 회원 혜택 등 코디터
                 마케팅 알림을 받겠습니다.
               </AlertText>
-              <AlertSwtich />
+              <AlertSwtich
+                onChange={handleMarketingAlert}
+                checked={marketingAlert}
+              />
             </CenterAlertContentsInner>
           </CenterAlertContentsEach2>
         </CenterContentsContainer>
