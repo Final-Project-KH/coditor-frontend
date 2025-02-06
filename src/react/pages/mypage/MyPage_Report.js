a90b63e7b614c42c400a339c36fba196c4a52859
qjawnsrla <santakim0303@gmail.com>
2025-02-06 11:25:35 +0900

    내 신고글 불러오기 위한 MyPage_Report.js 수정

--- src/react/pages/mypage/MyPage_Report.js
+++ src/react/pages/mypage/MyPage_Report.js
@@ -1,5 +1,5 @@
-import { useLocation, useNavigate, useOutletContext } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Wrap,
   TopBoxWide,
@@ -11,55 +11,49 @@ import {
   LeftContainer,
   CenterContainer,
 } from "../../styles/mypage/MyPage";
-
 import LeftTopProfile from "./Components/LeftTopProfile";
 import LeftMenus from "./Components/LeftMenus";
 import ScrollToTopButton from "../ScrollToTopButton";
-import { useEffect } from "react";
+import Board_PostList_MyPage_Report from "../community/components/common/Board_PostList_MyPage_Report";
 
 const MyPage_Report = () => {
   const navigate = useNavigate();
-  const { mainContentRef } = useOutletContext();
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(10);
 
-  // 페이지 진입 시 스크롤 위치 초기화
-  useEffect(() => {
-    if (mainContentRef?.current) {
-      mainContentRef.current.scrollTo(0, 0);
-    }
-  }, [mainContentRef]);
-
-  const handleMyPage = () => {
-    navigate("/mypage");
-  };
-
-  const handleRefresh = () => {
-    navigate("/mypage/report");
+  // ✅ 페이지 변경 함수 추가
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
   };
 
   return (
-    <>
-      <Wrap>
-        <TopBoxWide>
-          <TopBox>
-            <TopBoxLink onClick={() => handleMyPage()}>
-              <TopBoxText>my page</TopBoxText>
-            </TopBoxLink>
-            <TopBoxArrow>{`>`}</TopBoxArrow>
-            <TopBoxLink onClick={() => handleRefresh()}>
-              <TopBoxText>악성 사용자 신고</TopBoxText>
-            </TopBoxLink>
-          </TopBox>
-        </TopBoxWide>
-        <Container>
-          <LeftContainer>
-            <LeftTopProfile />
-            <LeftMenus />
-          </LeftContainer>
-          <CenterContainer></CenterContainer>
-        </Container>
-        <ScrollToTopButton />
-      </Wrap>
-    </>
+    <Wrap>
+      <TopBoxWide>
+        <TopBox>
+          <TopBoxLink onClick={() => navigate("/mypage")}>
+            <TopBoxText>my page</TopBoxText>
+          </TopBoxLink>
+          <TopBoxArrow>{`>`}</TopBoxArrow>
+          <TopBoxLink onClick={() => navigate("/mypage/report")}>
+            <TopBoxText>악성 사용자 신고</TopBoxText>
+          </TopBoxLink>
+        </TopBox>
+      </TopBoxWide>
+      <Container>
+        <LeftContainer>
+          <LeftTopProfile />
+          <LeftMenus />
+        </LeftContainer>
+        <CenterContainer>
+          <Board_PostList_MyPage_Report
+            page={page}
+            size={size}
+            onPageChange={handlePageChange}
+          />
+        </CenterContainer>
+      </Container>
+      <ScrollToTopButton />
+    </Wrap>
   );
 };
 
